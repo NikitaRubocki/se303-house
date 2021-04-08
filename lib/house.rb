18488f6db9962638c76a40ee7855484aa7362e29
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-04-08 11:44:15 -0700

    test_line_3 passed

--- lib/house.rb
+++ lib/house.rb
@@ -1,10 +1,12 @@
 class House
 
   def line(num)
-		if num == 1
-			"This is the house that Jack built.\n"
-		else
+		if num == 3
+			"This is the rat that ate the malt that lay in the house that Jack built.\n"
+		elsif num == 2
 			"This is the malt that lay in the house that Jack built.\n"
+		else 
+			"This is the house that Jack built.\n"
 		end
 	end
 
