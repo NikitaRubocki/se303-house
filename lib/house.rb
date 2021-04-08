afe462a10d45d1f62ca7566a120cc55ae025c547
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-04-08 11:52:45 -0700

    test_line_5 passed

--- lib/house.rb
+++ lib/house.rb
@@ -2,6 +2,8 @@ class House
 
   def line(num)
 		case num
+		when 5
+			"This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 4
 			"This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 3
