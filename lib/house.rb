c61518f9387b84299e6cc6d47a1ad755fe812ebd
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-04-08 11:51:55 -0700

    test_line_4 passed

--- lib/house.rb
+++ lib/house.rb
@@ -1,11 +1,14 @@
 class House
 
   def line(num)
-		if num == 3
+		case num
+		when 4
+			"This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+		when 3
 			"This is the rat that ate the malt that lay in the house that Jack built.\n"
-		elsif num == 2
+		when 2
 			"This is the malt that lay in the house that Jack built.\n"
-		else 
+		when 1
 			"This is the house that Jack built.\n"
 		end
 	end
