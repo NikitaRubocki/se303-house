db831b90a73bc30a2a23902fdaf09d0d25e76aaf
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-04-08 11:40:20 -0700

    test_line_2 passed

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,11 @@
 class House
 
-  def line(_)
-		"This is the house that Jack built.\n"
+  def line(num)
+		if num == 1
+			"This is the house that Jack built.\n"
+		else
+			"This is the malt that lay in the house that Jack built.\n"
+		end
 	end
-	
+
 end
