e652b20f05d5731b63f49f1be55945f66eb089b8
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-04-08 11:53:47 -0700

    test_line_7 passed

--- lib/house.rb
+++ lib/house.rb
@@ -2,6 +2,8 @@ class House
 
   def line(num)
 		case num
+		when 7
+			"This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 6
 			"This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 5
