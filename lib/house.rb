ffbac0b840dfbd8074c57b19f4e07004e66a72af
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-04-08 11:55:55 -0700

    test_line_9 passed

--- lib/house.rb
+++ lib/house.rb
@@ -2,6 +2,8 @@ class House
 
   def line(num)
 		case num
+		when 9
+			"This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 8
 			"This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 7
