abefcca878b4f4cd396f3fa91ee7bc4817d60945
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:37:59 -0700

    Add lines combo to case 8

--- lib/house.rb
+++ lib/house.rb
@@ -15,7 +15,7 @@ class House
 						 'the house that Jack built']
 		case num
 		when 8
-			"This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+			"This is #{lines.last(num).join(' ')}.\n"
 		when 9
 			"This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 10 
