49de49dacafa85dd93bc855587d03e0637fc3847
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:39:52 -0700

    Add rooster line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
 	end
 
   def line(num)
-		lines = ['the priest all shaven and shorn that married',
+		lines = ['the rooster that crowed in the morn that woke',
+						 'the priest all shaven and shorn that married',
 						 'the man all tattered and torn that kissed',
 						 'the maiden all forlorn that milked',
 						 'the cow with the crumpled horn that tossed',
