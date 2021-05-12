b1b9fb6288548436fb76405645eafb3d4a12b94a
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:43:20 -0700

    Add horse line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
 	end
 
   def line(num)
-		lines = ['the farmer sowing his corn that kept',
+		lines = ['the horse and the hound and the horn that belonged',
+						 'the farmer sowing his corn that kept',
 						 'the rooster that crowed in the morn that woke',
 						 'the priest all shaven and shorn that married',
 						 'the man all tattered and torn that kissed',
