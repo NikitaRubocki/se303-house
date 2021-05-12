41895783d3f4aae22755cfe76a811466c99ecd12
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:35:54 -0700

    Add lines combo to case 6

--- lib/house.rb
+++ lib/house.rb
@@ -13,7 +13,7 @@ class House
 						 'the house that Jack built']
 		case num
 		when 6
-			"This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+			"This is #{lines.last(num).join(' ')}.\n"
 		when 7
 			"This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 8
