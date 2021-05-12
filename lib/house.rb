825f476c7b91b00cb267b04ecd49c56ad613ff45
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:39:03 -0700

    Remove case 9

--- lib/house.rb
+++ lib/house.rb
@@ -15,8 +15,6 @@ class House
 			       'the malt that lay in', 
 						 'the house that Jack built']
 		case num
-		when 9
-			"This is #{lines.last(num).join(' ')}.\n"
 		when 10 
 			"This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 11
