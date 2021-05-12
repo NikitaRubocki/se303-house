c796e665242d9789a591d694c03239ccb6260471
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:37:39 -0700

    Add man line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
 	end
 
   def line(num)
-		lines = ['the maiden all forlorn that milked',
+		lines = ['the man all tattered and torn that kissed',
+						 'the maiden all forlorn that milked',
 						 'the cow with the crumpled horn that tossed',
 						 'the dog that worried',
 						 'the cat that killed',
