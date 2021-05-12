9aa442c8cd511ba815b56670afa81f102e33ac02
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:41:08 -0700

    Add farmer line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
 	end
 
   def line(num)
-		lines = ['the rooster that crowed in the morn that woke',
+		lines = ['the farmer sowing his corn that kept',
+						 'the rooster that crowed in the morn that woke',
 						 'the priest all shaven and shorn that married',
 						 'the man all tattered and torn that kissed',
 						 'the maiden all forlorn that milked',
