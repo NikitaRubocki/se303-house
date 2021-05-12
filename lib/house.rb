48e9d0b7c7fa4fad4f05566dc83079eab25b479f
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:29:47 -0700

    Add else statement to switch statement, put the now identical case 1 and case 2 in the else

--- lib/house.rb
+++ lib/house.rb
@@ -7,10 +7,10 @@ class House
   def line(num)
 		lines = ['the malt that lay in', 'the house that Jack built']
 		case num
-		when 1
-			"This is #{lines.last(num).join(' ')}.\n"
-		when 2
-			"This is #{lines.last(num).join(' ')}.\n"
+		# when 1
+		# 	"This is #{lines.last(num).join(' ')}.\n"
+		# when 2
+		# 	"This is #{lines.last(num).join(' ')}.\n"
 		when 3
 			"This is the rat that ate the malt that lay in the house that Jack built.\n"
 		when 4
@@ -31,7 +31,10 @@ class House
 			"This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 12
 			"This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+		else
+			"This is #{lines.last(num).join(' ')}.\n"
 		end
+		
 	end
 
 	
