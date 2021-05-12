01f2b337d73f221a36f832701f6638e1e8824d7a
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:34:24 -0700

    Add dog line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
 	end
 
   def line(num)
-		lines = ['the cat that killed',
+		lines = ['the dog that worried',
+						 'the cat that killed',
 			       'the rat that ate', 
 			       'the malt that lay in', 
 						 'the house that Jack built']
