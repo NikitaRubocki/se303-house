1ea816c3928600a580d5d6c76638441354b0ce06
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:04:19 -0700

    Replace duplicate line in num 2 case with line from lines list

--- lib/house.rb
+++ lib/house.rb
@@ -10,7 +10,7 @@ class House
 		when 1
 			"This is #{lines[0]}.\n"
 		when 2
-			"This is the malt that lay in the house that Jack built.\n"
+			"This is the malt that lay in #{lines[0]}.\n"
 		when 3
 			"This is the rat that ate the malt that lay in the house that Jack built.\n"
 		when 4
