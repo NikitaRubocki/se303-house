002b934989af1b87560fc21239931cea5f71abf6
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:38:32 -0700

    Add priest line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
 	end
 
   def line(num)
-		lines = ['the man all tattered and torn that kissed',
+		lines = ['the priest all shaven and shorn that married',
+						 'the man all tattered and torn that kissed',
 						 'the maiden all forlorn that milked',
 						 'the cow with the crumpled horn that tossed',
 						 'the dog that worried',
