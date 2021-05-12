7b4323290ee2a6b5079be162794dfdede08cc0d8
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-12 10:44:03 -0700

    Add lines combo to case 12

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,7 @@ class House
 	end
 
   def line(num)
-		lines = ['the horse and the hound and the horn that belonged',
+		lines = ['the horse and the hound and the horn that belonged to',
 						 'the farmer sowing his corn that kept',
 						 'the rooster that crowed in the morn that woke',
 						 'the priest all shaven and shorn that married',
@@ -19,7 +19,7 @@ class House
 						 'the house that Jack built']
 		case num
 		when 12
-			"This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+			"This is #{lines.last(num).join(' ')}.\n"
 		else
 			"This is #{lines.last(num).join(' ')}.\n"
 		end
