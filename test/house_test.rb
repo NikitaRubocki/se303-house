e636c677b988550bdc936b5e7ae4992a09740bc4
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-18 16:55:45 -0700

    Add a few more tests for randomness

--- test/house_test.rb
+++ test/house_test.rb
@@ -118,6 +118,16 @@ This is the horse and the hound and the horn that belonged to the farmer sowing 
       expected = "This is the house that Jack built the rat that ate the cat that killed the maiden all forlorn that milked.\n"
       assert_equal expected, House.new(random: true, seed: 1).line(4)
     end
+
+    def test_line_5
+      expected = "This is the dog that worried the house that Jack built the rat that ate the cat that killed the maiden all forlorn that milked.\n"
+      assert_equal expected, House.new(random: true, seed: 1).line(5)
+    end
+
+    def test_line_6
+      expected = "This is the horse and the hound and the horn that belonged to the dog that worried the house that Jack built the rat that ate the cat that killed the maiden all forlorn that milked.\n"
+      assert_equal expected, House.new(random: true, seed: 1).line(6)
+    end
   end
 
 end
