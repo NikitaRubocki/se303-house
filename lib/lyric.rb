0b4bf6ef2753758d55cd538522f987734a13e737
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-18 18:27:10 -0700

    Remove lines method from BasicLyric and RandomLyric

--- lib/lyric.rb
+++ lib/lyric.rb
@@ -40,25 +40,6 @@ class BasicLyric < Lyric
 		"#{@start} #{lines.last(@num).join(' ')}.\n"
 	end
 
-	# private
-	
-	# def lines
-	# 	[
-	# 		'the horse and the hound and the horn that belonged to',
-	# 		'the farmer sowing his corn that kept',
-	# 		'the rooster that crowed in the morn that woke',
-	# 		'the priest all shaven and shorn that married',
-	# 		'the man all tattered and torn that kissed',
-	# 		'the maiden all forlorn that milked',
-	# 		'the cow with the crumpled horn that tossed',
-	# 		'the dog that worried',
-	# 		'the cat that killed',
-	# 		'the rat that ate', 
-	# 		'the malt that lay in', 
-	# 		'the house that Jack built'
-	# 	]
-	# end
-
 end
 
 class RandomLyric < Lyric
@@ -73,25 +54,6 @@ class RandomLyric < Lyric
 	def line
 		pieces = lines.shuffle(random: Random.new(@seed))
 		"#{@start} #{pieces.last(@num).join(' ')}.\n"
-	end
-
-	private
-	
-	def lines
-		[
-			'the horse and the hound and the horn that belonged to',
-			'the farmer sowing his corn that kept',
-			'the rooster that crowed in the morn that woke',
-			'the priest all shaven and shorn that married',
-			'the man all tattered and torn that kissed',
-			'the maiden all forlorn that milked',
-			'the cow with the crumpled horn that tossed',
-			'the dog that worried',
-			'the cat that killed',
-			'the rat that ate', 
-			'the malt that lay in', 
-			'the house that Jack built'
-		]
 	end
 
 end
