70a809521a219f69cd1ee65658dd23c13ee189ec
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-18 17:41:09 -0700

    Add lines method and add functionality to line method in RandomLyric

--- lib/lyric.rb
+++ lib/lyric.rb
@@ -54,7 +54,27 @@ class RandomLyric < Lyric
 	end
 
 	def line
+		pieces = lines.shuffle(random: Random.new(@seed))
+		"#{@start} #{pieces.last(@num).join(' ')}.\n"
+	end
 
+	private
+	
+	def lines
+		[
+			'the horse and the hound and the horn that belonged to',
+			'the farmer sowing his corn that kept',
+			'the rooster that crowed in the morn that woke',
+			'the priest all shaven and shorn that married',
+			'the man all tattered and torn that kissed',
+			'the maiden all forlorn that milked',
+			'the cow with the crumpled horn that tossed',
+			'the dog that worried',
+			'the cat that killed',
+			'the rat that ate', 
+			'the malt that lay in', 
+			'the house that Jack built'
+		]
 	end
 
 end
