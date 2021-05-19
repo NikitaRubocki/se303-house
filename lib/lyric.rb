125cb93da937f2037a102ea22dd0285cc24f2cd4
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-18 18:26:21 -0700

    Remove line and init function from lyric

--- lib/lyric.rb
+++ lib/lyric.rb
@@ -1,22 +1,11 @@
 class Lyric
 
-	def initialize(num, start, random=false, seed=1)
-		@num = num
-		@start = start
-		@random = random
-		@seed = seed
-	end
-
 	def self.for(num, start, random, seed)
 		if random == true
 			RandomLyric.new(num, start, random, seed)
 		else
 			BasicLyric.new(num, start)
 		end
-	end
-
-	def line
-		"#{@start} #{lines.last(@num).join(' ')}.\n"
 	end
 
 	private
@@ -51,24 +40,24 @@ class BasicLyric < Lyric
 		"#{@start} #{lines.last(@num).join(' ')}.\n"
 	end
 
-	private
+	# private
 	
-	def lines
-		[
-			'the horse and the hound and the horn that belonged to',
-			'the farmer sowing his corn that kept',
-			'the rooster that crowed in the morn that woke',
-			'the priest all shaven and shorn that married',
-			'the man all tattered and torn that kissed',
-			'the maiden all forlorn that milked',
-			'the cow with the crumpled horn that tossed',
-			'the dog that worried',
-			'the cat that killed',
-			'the rat that ate', 
-			'the malt that lay in', 
-			'the house that Jack built'
-		]
-	end
+	# def lines
+	# 	[
+	# 		'the horse and the hound and the horn that belonged to',
+	# 		'the farmer sowing his corn that kept',
+	# 		'the rooster that crowed in the morn that woke',
+	# 		'the priest all shaven and shorn that married',
+	# 		'the man all tattered and torn that kissed',
+	# 		'the maiden all forlorn that milked',
+	# 		'the cow with the crumpled horn that tossed',
+	# 		'the dog that worried',
+	# 		'the cat that killed',
+	# 		'the rat that ate', 
+	# 		'the malt that lay in', 
+	# 		'the house that Jack built'
+	# 	]
+	# end
 
 end
 
