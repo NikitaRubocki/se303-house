dcfa8dc8bb66b6287f714197d7f3c955f1dfb6ff
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-05-24 14:35:41 -0700

    Create static DATA variable, which data method uses

--- lib/house.rb
+++ lib/house.rb
@@ -17,23 +17,26 @@ class House
 	end
 
 	private
+
+	DATA = [
+		'the horse and the hound and the horn that belonged to',
+		'the farmer sowing his corn that kept',
+		'the rooster that crowed in the morn that woke',
+		'the priest all shaven and shorn that married',
+		'the man all tattered and torn that kissed',
+		'the maiden all forlorn that milked',
+		'the cow with the crumpled horn that tossed',
+		'the dog that worried',
+		'the cat that killed',
+		'the rat that ate', 
+		'the malt that lay in', 
+		'the house that Jack built'
+	]
 	
 	def data
-		[
-			'the horse and the hound and the horn that belonged to',
-			'the farmer sowing his corn that kept',
-			'the rooster that crowed in the morn that woke',
-			'the priest all shaven and shorn that married',
-			'the man all tattered and torn that kissed',
-			'the maiden all forlorn that milked',
-			'the cow with the crumpled horn that tossed',
-			'the dog that worried',
-			'the cat that killed',
-			'the rat that ate', 
-			'the malt that lay in', 
-			'the house that Jack built'
-		]
+		DATA
 	end
+
 
 end
 
@@ -41,5 +44,5 @@ class RandomHouse < House
 
 	def data
 	end
-	
+
 end
